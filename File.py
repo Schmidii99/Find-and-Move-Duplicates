7b2d490bddfbe5ff2a41d6635fb3f3aa333f99e2
Florian Schmid <70525898+Schmidii99@users.noreply.github.com>
2024-01-01 22:58:29 +0100

    Initial Commit

---
+++ File.py
@@ -0,0 +1,31 @@
+import hashlib
+import os
+
+
+class MyFile:
+    def __init__(self, name, path, file_size, file_hash):
+        self.name = name
+        self.path = path
+        self.file_size = file_size
+        self.file_hash = file_hash
+
+    @staticmethod
+    def from_path(file_path) -> "MyFile":
+        file_hash = hashlib.md5((open(file_path, "rb").read())).hexdigest()
+        file_name = os.path.basename(file_path)
+        file_size = os.path.getsize(file_path)
+        return MyFile(file_name, file_path, file_size, file_hash)
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, MyFile):
+            if self.name != other.name:
+                return False
+            if self.name != other.name:
+                return False
+            if self.name != other.name:
+                return False
+            if self.name != other.name:
+                return False
+            return True
+        else:
+            return False
---
+++ main.py
@@ -0,0 +1,56 @@
+import time
+from pprint import pprint
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+
+import os
+import shutil
+
+import hashlib
+from File import MyFile
+
+
+def iterate_folder(path, existing_files: dict) -> dict:
+    walker = os.walk(path)
+    for folder, sub_folders, files in walker:
+        for file in files:
+            print(f"Found file {file}")
+            existing_files[file] = MyFile.from_path(os.path.join(folder, file))
+        for sub_folder in sub_folders:
+            existing_files = iterate_folder(sub_folder, existing_files)
+
+    return existing_files
+
+
+def move_duplicates(path, uniqe_files: dict, destination):
+    walker = os.walk(path)
+    for folder, sub_folders, files in walker:
+        for file in files:
+            file_path = os.path.join(folder, file)
+            files_obj = uniqe_files.get(file)
+            if files_obj is not None:
+                if files_obj == MyFile.from_path(file_path):
+                    print(f"Moved identical file: {file_path}")
+                    shutil.move(file_path, os.path.join(destination, file))
+
+        for sub_folder in sub_folders:
+            move_duplicates(sub_folder, uniqe_files, destination)
+
+
+if __name__ == '__main__':
+    Tk().withdraw()
+
+    # The files in this folder won't be moved
+    original = askdirectory(title="Select the origin folder")
+    # The files in this folder WILL be compared and moved
+    addition = askdirectory(title="Select the additional folder")
+    # duplicate files will be moved from addition to this folder
+    destination = askdirectory(title="Select the additional folder")
+
+    files_dict = dict()
+    files_dict = iterate_folder(original, files_dict)
+    print("--------------------------------------------------")
+    time.sleep(3)
+    move_duplicates(addition, files_dict, destination)
+    print("--------------------------------------------------")
+    print("Done")
